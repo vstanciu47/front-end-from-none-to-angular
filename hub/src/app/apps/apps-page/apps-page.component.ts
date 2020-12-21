e81819df776be6d6824e949e326b039dad6ab937
Augustin Braslasu <Augustin_Braslasu@waters.com>
2020-12-21 18:31:39 +0200

    refactor using RxJS

--- hub/src/app/apps/apps-page/apps-page.component.ts
+++ hub/src/app/apps/apps-page/apps-page.component.ts
@@ -1,27 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, combineLatest, EMPTY, of, Subject } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { AppsService, Launcher } from '../apps.service';
 
 @Component({
   selector: 'app-apps-page',
   templateUrl: './apps-page.component.html',
-  styleUrls: ['./apps-page.component.scss']
+  styleUrls: ['./apps-page.component.scss'],
 })
 export class AppsPageComponent implements OnInit {
+  pageName = 'Apps page';
 
-  pageName = "Apps page";
+  error$ = new Subject<string>();
 
-  launchers = new Array<Launcher>();
+  appTypes$ = this.appsService.appTypes$;
 
-  constructor(
-    private appsService: AppsService
-  ) { }
+  launcherTypeSelected$ = new BehaviorSubject<number>(0);
+
+  // launchers = new Array<Launcher>();
+  launchers$ = this.appsService.launchersWithTypes$.pipe(
+    catchError((error) => {
+      this.error$.next(error);
+      return EMPTY;
+    })
+  );
+
+  filteredLaunchers$ = combineLatest([
+    this.launchers$,
+    this.launcherTypeSelected$,
+  ]).pipe(
+    map(
+      ([launchers, appType]) =>
+        launchers.filter((launcher) =>
+          appType !== 0 ? launcher.typeId === appType : true
+        )
+    )
+  );
+
+  constructor(private appsService: AppsService) {}
 
   ngOnInit(): void {
-    this.appsService.getApps().then(launchers => this.launchers = launchers);
+    // this.appsService.getApps().then(launchers => this.launchers = launchers);
   }
 
   launched($event: Launcher) {
-    console.log("AppsPageComponent.launched", { $event });
+    console.log('AppsPageComponent.launched', { $event });
   }
 
+  typeSelected(target: any) {
+    let appType = <HTMLTextAreaElement>target;
+    this.launcherTypeSelected$.next(+appType.value);
+  }
 }
